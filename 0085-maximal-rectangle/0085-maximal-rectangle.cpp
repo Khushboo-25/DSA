6ce5eeb4bcdf5bd1f4541992608fbc9cffa3c105
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-10-17 18:28:29 +0530

    Time: 12 ms (99.93%), Space: 17.7 MB (78.95%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+int f(vector<int>&height){
+    int n = height.size();
+    int leftsmall[n],rightsmall[n];
+    stack<int> st;
+    for(int i=0;i<n;i++){
+        while(!st.empty() && height[st.top()]>=height[i]){
+            st.pop();
+        }
+        if(st.empty()) leftsmall[i]=0;
+        else 
+        leftsmall[i] = st.top()+1;
+        st.push(i);
+    }
+    while(!st.empty()) st.pop();
+
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty() && height[st.top()]>=height[i]){
+            st.pop();
+        }
+        if(st.empty()) rightsmall[i]=n-1;
+        else rightsmall[i] = st.top()-1;
+        st.push(i);
+    }
+    int maxans =0;
+    for(int i=0;i<n;i++){
+        maxans=max(maxans,height[i]*(rightsmall[i]-leftsmall[i]+1));
+    }
+    return maxans;
+}
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int maxa=0;
+        vector<int> heights(m,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]=='1') heights[j]++;
+                else heights[j]=0;
+            }
+            int area = f(heights);
+            maxa=max(area,maxa);
+        }
+      return maxa;
+    }
+};
