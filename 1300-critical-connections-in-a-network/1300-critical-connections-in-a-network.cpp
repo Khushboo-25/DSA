6cf52f873fb575807d05745ab5c8c96723007047
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-10-20 05:30:16 +0530

    Time: 199 ms (99.68%), Space: 182.2 MB (34.26%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
@@ -1,46 +1,38 @@
 class Solution {
 public:
 int time=1;
-void dfs(int node,int p,vector<int> &vv,int ta[],int low[],vector<vector<int>> &adj, vector<vector<int>> &bridges)
+void dfs(int i,int p,vector<int> &vv,vector<int> &ta,vector<int> &low,vector<vector<int>> &adj,vector<vector<int>>&ans)
 {
-    vv[node]=1;
-    ta[node]=time;
-    low[node]=time;
+    vv[i]=1;
+    ta[i]=time;
+    low[i]=time;
     time++;
-    for(auto it:adj[node])
+    for(auto it: adj[i])
     {
         if(it==p)
-        {
-            continue;
-        }
+        continue;
         else if(vv[it]==1)
-        {
-            low[node]=min({low[it],low[node]});
-        }
-        else
-        {
-            dfs(it,node,vv,ta,low,adj,bridges);
-            low[node]=min({low[it],low[node]});
-            if(low[it]>ta[node])
-            {
-                bridges.push_back({it,node});
-            }
+        low[i]=min(low[i],low[it]);
+        else{
+            dfs(it,i,vv,ta,low,adj,ans);
+            low[i]=min(low[i],low[it]);
+            if(ta[i]<low[it])
+            ans.push_back({i,it});
         }
     }
 }
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& conn) {
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& conn) 
+    {
         vector<vector<int>> adj(n);
-        for(auto it:conn)
+        for(auto it: conn)
         {
-            adj[it[1]].push_back(it[0]);
             adj[it[0]].push_back(it[1]);
-
+            adj[it[1]].push_back(it[0]);
         }
-        vector<int> vv(n,0);
-        int ta[n];
-        int low[n];
-        vector<vector<int>> bridges;
-        dfs(0,-1,vv,ta,low,adj,bridges);
-        return bridges;
+        vector<int> vv(n,0),ta(n,0),low(n,0);
+        vector<vector<int>> ans;
+        dfs(0,-1,vv,ta,low,adj,ans);
+        return ans;
+        
     }
 };
