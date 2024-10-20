92d70361497de964e5847d790fb7b1b89a0b65f7
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-10-20 19:23:41 +0530

    Time: 88 ms (42.46%), Space: 47.3 MB (43.48%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& ad, int V, int S) {
-        vector<vector<vector<int>>>adj(V+1);
-        for(auto it: ad)
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) 
+    {
+        vector<vector<pair<int,int>>>adj(n+1);
+        for(auto it:times)
         {
             int x=it[0];
             int y=it[1];
-            int d=it[2];
-            adj[x].push_back({y,d});
+            int tm=it[2];
+            adj[x].push_back({y,tm});
         }
-        vector<int> vv(V+1, INT_MAX); // Initialize distances to infinity
-        vv[S] = 0; // Set distance of the source to 0 (1-based to 0-based index)
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        pq.push({0,S});
+        vector<int>tt(n+1,INT_MAX);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        pq.push({0,k});
+        tt[k]=0;
         while(!pq.empty())
         {
-            auto it=pq.top();
-            int d=it.first;
-            int node=it.second;
+            auto tp=pq.top();
+            int node=tp.second;
+            int d=tp.first;
             pq.pop();
-            for(auto nw: adj[node])
+            for(auto it:adj[node])
             {
-                int n1=nw[0];
-                int d2=nw[1];
-                if(d+d2<vv[n1])
+                int nw=it.first;
+                int d2=it.second;
+                if(tt[nw]>d+d2)
                 {
-                    vv[n1]=d+d2;
-                    pq.push({vv[n1],n1});
+                    tt[nw]=d+d2;
+                    pq.push({tt[nw],nw});
                 }
             }
+        }
+        int ans=0;
+        for(int i=1;i<=n;i++)
+        {
+            ans=max(tt[i],ans);
+        }
+        if(ans==INT_MAX)
+        return -1;
+        return ans;
 
-        }
-        // Find the maximum distance
-        int maxDist = 0;
-        for (int i = 1 ;i <=V; ++i) {
-            if (vv[i] == INT_MAX) { // If any node is unreachable, return -1
-                return -1;
-            }
-            maxDist = max(maxDist, vv[i]);
-        }
         
-        return maxDist; // Maximum time it takes for all nodes to receive the signal
     }
 };
