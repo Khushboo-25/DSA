68afe96c2eb86a976f6cead331251c954b43d7cf
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-10-21 19:34:16 +0530

    Time: 2387 ms (5%), Space: 32.6 MB (16.57%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,65 @@
+#define point pair<int,pair<int,int>>
+vector<pair<int,int>>dr={{-1,0},{1,0},{0,1},{0,-1}};
+class Solution {
+public:
+int call(int i,int j,vector<vector<int>>& mat,int &n,int &m,vector<vector<int>>&vv)
+{
+    // vector<vector<int>>vv(n,vector<int>(m,0));
+    // return call2(i,j,n,m,vv,mat);
+    int ans=0;
+    
+    priority_queue<point>pq;
+    pq.push({1,{i,j}});
+    vv[i][j]=1;
+    while(!pq.empty())
+    {
+        auto it=pq.top();
+        int d=it.first;
+        int x=it.second.first;
+        int y=it.second.second;
+        pq.pop();
+        
+        for(auto it1:dr)
+        {
+            int x1=x+it1.first;
+            int y1=y+it1.second;
+            if(x1<n && x1>=0 && y1>=0 && y1<m && mat[x1][y1]>mat[x][y])
+            {
+                if(vv[x1][y1]<d+1)
+                {
+                    vv[x1][y1]=d+1;
+                    pq.push({vv[x1][y1],{x1,y1}});
+                }
+            }
+
+        }
+       
+    }
+     for(int k=0;k<n;k++)
+        {
+            for(int l=0;l<m;l++)
+            {
+                ans=max(ans,vv[k][l]);
+            }
+        }
+        
+    return ans;
+
+
+}
+    int longestIncreasingPath(vector<vector<int>>& mat) 
+    {
+        int ans=0;
+        int n=mat.size(),m=mat[0].size();
+        
+        vector<vector<int>>vv(n,vector<int>(m,-1));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                ans=max(ans,call(i,j,mat,n,m,vv));
+            }
+        }
+        return ans;
+    }
+};
