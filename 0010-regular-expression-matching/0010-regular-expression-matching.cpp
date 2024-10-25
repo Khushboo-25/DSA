417711aeaa6340ad7aa8674f1119c004c7ba86f9
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-10-26 00:48:05 +0530

    Time: 0 ms (100%), Space: 8.8 MB (63.21%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+bool helper(int i,int j,string &s,string &p,vector<vector<int>>&dp){
+    
+    int n=s.size(),m=p.size();
+    if(j>=m){
+        return (i>=n);
+    }
+    if(dp[i][j]!=-1)return dp[i][j];
+    bool nextStar=false;
+    if(j+1<m && p[j+1]=='*')
+    nextStar=true;
+    if(nextStar){
+        bool a=false,b=false;
+        if(i<n &&  (s[i]==p[j] || p[j]=='.')  )
+        a=helper(i+1,j,s,p,dp);
+
+        b=helper(i,j+2,s,p,dp);
+        return dp[i][j]= (a|b);
+    }
+    else{
+        if(i<n && (s[i]==p[j]||p[j]=='.'))
+        return dp[i][j]= helper(i+1,j+1,s,p,dp);
+    }
+    return dp[i][j]= false;
+}
+    bool isMatch(string s, string p) {
+        vector<vector<int>>dp(s.size()+1,vector<int>(p.size()+1,-1));
+        return helper(0,0,s,p,dp);
+    }
+};
