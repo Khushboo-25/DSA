755f7337ec881040abb6c13a63c3b056209d0453
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-10-25 12:56:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -27,6 +27,20 @@ int call(int i,int csum,int n,int sum,vector<int> &a,vector<vector<int>>&dp)
 
         int n=a.size();
          vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-        return call(0,0,n,sum,a,dp);
+         for(int i=0;i<=n;i++)
+         dp[i][0]=1;
+         for(int i=1;i<=sum;i++)
+         dp[0][i]=0;
+         for(int i=1;i<=n;i++)
+         {
+            for(int j=1;j<=sum;j++)
+            {
+                if(a[i-1]<=j)
+                dp[i][j]=dp[i][j-a[i-1]]+dp[i-1][j];
+                else
+                dp[i][j]=dp[i-1][j];
+            }
+         }
+        return dp[n][sum];
     }
 };
