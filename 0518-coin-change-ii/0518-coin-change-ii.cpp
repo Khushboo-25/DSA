8acceced7be32f9d8970d907e359f3e5b76a515e
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-10-25 18:28:28 +0530

    Time: 36 ms (15.26%), Space: 27.6 MB (18.44%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
 // int MOD=1e9+7;
-int call(int i,int csum,int n,int sum,vector<int> &a,vector<vector<int>>&dp)
+int call(int i,int sum,int &n,vector<int> &a,vector<vector<int>>&dp)
 {
-    if(csum==sum)
+    if(sum==0)
     return 1;
     
-    if(i>=n || csum>sum)
+    if(i>=n )
     return 0;
     
-    if(dp[i][csum]!=-1)
-    return dp[i][csum];
+    if(dp[i][sum]!=-1)
+    return dp[i][sum];
     int ways=0;
-    if(a[i]<=sum-csum)
+    if(a[i]<=sum)
     {
-        ways=(call(i,csum+a[i],n,sum,a,dp)+call(i+1,csum,n,sum,a,dp));
+        int take=call(i,sum-a[i],n,a,dp);
+        ways=take+ways;
     }
-    else
-    {
-        ways=call(i+1,csum,n,sum,a,dp);
-    }
-    return dp[i][csum]=ways;
-}
 
+    int nottake=call(i+1,sum,n,a,dp);
+    ways=nottake+ways;
+    return dp[i][sum]=ways;
+}
     int change(int sum, vector<int>& a) {
 
         int n=a.size();
-         vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-        return call(0,0,n,sum,a,dp);
+        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
+        int ways=call(0,sum,n,a,dp);
+        
+        return ways;
     }
 };
