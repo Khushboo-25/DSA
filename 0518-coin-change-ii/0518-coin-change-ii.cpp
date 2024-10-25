6012dd51319e05f296bad59059c6888aa8350243
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-10-25 15:02:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-// int MOD=1e9+7;
-int call(int i,int csum,int n,int sum,vector<int> &a,vector<vector<int>>&dp)
-{
-    if(csum==sum)
-    return 1;
-    
-    if(i>=n || csum>sum)
-    return 0;
-    
-    if(dp[i][csum]!=-1)
-    return dp[i][csum];
-    int ways=0;
-    if(a[i]<=sum-csum)
-    {
-        ways=(call(i,csum+a[i],n,sum,a,dp)+call(i+1,csum,n,sum,a,dp));
-    }
-    else
-    {
-        ways=call(i+1,csum,n,sum,a,dp);
-    }
-    return dp[i][csum]=ways;
-}
 
-    int change(int sum, vector<int>& a) {
+    int change(int tt, vector<int>& coins) {
 
-        int n=a.size();
-         vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-        return call(0,0,n,sum,a,dp);
+        int n=coins.size();
+        vector<vector<int> > pp(n+1,vector<int> (tt+1,0));
+        for(int i=0;i<=n;i++) pp[i][0]=1;
+        for(int i=1;i<=tt;i++) pp[0][i]=0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=tt;j++)
+            {
+                if(coins[i-1]<=j)
+                {
+                    pp[i][j]=pp[i][j-coins[i-1]]+pp[i-1][j];
+                }
+                else{
+                    pp[i][j]=pp[i-1][j];
+                }
+            }
+        }
+        return pp[n][tt];
     }
 };
