14b3693a8dd665ed6d624a5e1e4ae6295c3c19bd
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-10-25 17:19:54 +0530

    Time: 384 ms (11.44%), Space: 157 MB (25.31%) - LeetHub

--- 3106-length-of-the-longest-subsequence-that-sums-to-target/3106-length-of-the-longest-subsequence-that-sums-to-target.cpp
+++ 3106-length-of-the-longest-subsequence-that-sums-to-target/3106-length-of-the-longest-subsequence-that-sums-to-target.cpp
@@ -6,27 +6,30 @@ int call(int i,int &n,vector<int> &nums,int tt,vector<vector<int>>&dp)
     {
         return 0;
     }
-    if(i>=n)
-    return -1e5;
-    if(dp[tt][i]!=-1)
-    return dp[tt][i];
-    int len=-1e5;
+    if(i>=n )
+    return INT_MIN;
+    if(dp[i][tt]!=INT_MIN)
+    return dp[i][tt];
+    int len=INT_MIN;
     if(nums[i]<=tt)
     {
         int len1=call(i+1,n,nums,tt-nums[i],dp);
-        len=max(len,len1+1);
+        
+        len=max(len1+1,len);
+
+        // len=max(len,len1+1);
     }
-
     int len2=call(i+1,n,nums,tt,dp);
+    // if(len2!=-1)
     len=max(len,len2);
-
-    return dp[tt][i]=len;
+    return dp[i][tt]=len;
 
 }
     int lengthOfLongestSubsequence(vector<int>& nums, int target) {
         int n=nums.size();
         // int ans=-1;
-        vector<vector<int>>dp(target+1,vector<int>(n+1,-1));
+        vector<vector<int>>dp(n+1,vector<int> (target+1,INT_MIN));
+
         int ans=call(0,n,nums,target,dp);
         if(ans>0)
         return ans;
