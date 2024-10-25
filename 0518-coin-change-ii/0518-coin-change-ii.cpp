e2f88e5481f955b81bc76fe6a784f52a75969c3e
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-10-25 13:00:41 +0530

    Time: 93 ms (8.65%), Space: 246.5 MB (5.11%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,46 +1,16 @@
 class Solution {
 public:
-// int MOD=1e9+7;
-int call(int i,int csum,int n,int sum,vector<int> &a,vector<vector<int>>&dp)
-{
-    if(csum==sum)
-    return 1;
-    
-    if(i>=n || csum>sum)
-    return 0;
-    
-    if(dp[i][csum]!=-1)
-    return dp[i][csum];
-    int ways=0;
-    if(a[i]<=sum-csum)
-    {
-        ways=(call(i,csum+a[i],n,sum,a,dp)+call(i+1,csum,n,sum,a,dp));
+    int calc(int amount, vector<int>& coins,int i,vector<vector<int>>&dp){
+        if(i<0||amount<0)
+            return 0;
+        if(amount==0)
+            return 1;
+        if(dp[i][amount]!=-1)
+            return dp[i][amount];
+       return dp[i][amount]=calc(amount-coins[i],coins,i,dp)+calc(amount,coins,i-1,dp);
     }
-    else
-    {
-        ways=call(i+1,csum,n,sum,a,dp);
-    }
-    return dp[i][sum]=ways;
-}
-
-    int change(int sum, vector<int>& a) {
-
-        int n=a.size();
-         vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-         for(int i=0;i<=n;i++)
-         dp[i][0]=1;
-         for(int i=1;i<=sum;i++)
-         dp[0][i]=0;
-         for(int i=1;i<=n;i++)
-         {
-            for(int j=1;j<=sum;j++)
-            {
-                if(a[i-1]<=j)
-                dp[i][j]=dp[i][j-a[i-1]]+dp[i-1][j];
-                else
-                dp[i][j]=dp[i-1][j];
-            }
-         }
-        return dp[n][sum];
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int>>dp(301,vector<int>(5001,-1));
+        return calc(amount,coins,coins.size()-1,dp);
     }
 };
