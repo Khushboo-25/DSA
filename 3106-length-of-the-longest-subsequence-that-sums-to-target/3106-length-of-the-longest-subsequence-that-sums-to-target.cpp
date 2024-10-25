3c915ac5fc11de9ea7c1409e1929c799acfe6a2f
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-10-25 17:11:38 +0530

    Time: 417 ms (8.54%), Space: 169 MB (10.67%) - LeetHub

--- 3106-length-of-the-longest-subsequence-that-sums-to-target/3106-length-of-the-longest-subsequence-that-sums-to-target.cpp
+++ 3106-length-of-the-longest-subsequence-that-sums-to-target/3106-length-of-the-longest-subsequence-that-sums-to-target.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
-    int n;
-    int solve(int target, int ind, vector<int> &nums, vector<vector<int>> &dp){
-        // base
-        if(target == 0){
-            return 0;
-        }
-        if(ind == n){
-            return -1e5;
-        }
-        if(dp[target][ind] != -1) return dp[target][ind];
-        // take
-        int take = -1e5;
-        if(target >= nums[ind]){
-            take = 1 + solve(target-nums[ind],ind+1,nums,dp);
-        }
-        //nottake
-        int nottake = solve(target,ind+1,nums,dp);
-        return dp[target][ind] = max(take,nottake);
+int call(int i,int &n,vector<int> &nums,int tt,vector<vector<int>>&dp)
+{
+    if(tt<=0)
+    {
+        return 0;
     }
-    int lengthOfLongestSubsequence(vector<int>& nums, int k) {
-        n= nums.size();
-        vector<vector<int>> dp(k+1,vector<int>(n+1,-1));
-        int ans = solve(k,0,nums,dp);
-        return ans > 0 ? ans : -1;
+    if(i>=n)
+    return -1e5;
+    if(dp[tt][i]!=-1)
+    return dp[tt][i];
+    int len=-1e5;
+    if(nums[i]<=tt)
+    {
+        int len1=call(i+1,n,nums,tt-nums[i],dp);
+        len=max(len,len1+1);
+    }
+
+    int len2=call(i+1,n,nums,tt,dp);
+    len=max(len,len2);
+
+    return dp[tt][i]=len;
+
+}
+    int lengthOfLongestSubsequence(vector<int>& nums, int target) {
+        int n=nums.size();
+        // int ans=-1;
+        vector<vector<int>>dp(target+1,vector<int>(n+1,-1));
+        int ans=call(0,n,nums,target,dp);
+        if(ans>0)
+        return ans;
+        return -1;
+        
+        // return ans;
+        
     }
 };
