d1d4451eabcb9ae8f5542c48cf6c3e68c17ce703
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-10-26 13:42:30 +0530

    Time: 2 ms (74.69%), Space: 8.7 MB (43.07%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
-    int n1, n2, n3;
+int n1,n2,n3;
+bool call(int i,int j,int l,string s1,string s2,string s3,vector<vector<vector<int>>>&dp)
+{
+    if(j==n1)
+    return s3.substr(i)==s2.substr(l);
+    else if(l==n2)
+    return s3.substr(i)==s1.substr(j);
+    if(dp[i][j][l]!=-1)
+    return dp[i][j][l];
+    if(s1[j]!=s2[l])
+    {
+        if(s1[j]==s3[i])
+        return dp[i][j][l]=call(i+1,j+1,l,s1,s2,s3,dp);
+        else if(s2[l]==s3[i])
+        return dp[i][j][l]=call(i+1,j,l+1,s1,s2,s3,dp);
+        else 
+        return dp[i][j][l]=0;
+    }
+    else
+    {
+        if(s3[i]!=s1[j]) 
+        return dp[i][j][l]=0;
+        return dp[i][j][l]=call(i+1,j+1,l,s1,s2,s3,dp)|call(i+1,j,l+1,s1,s2,s3,dp);
+
+    }
+}
 
     bool isInterleave(string s1, string s2, string s3) {
         n1 = s1.size();
