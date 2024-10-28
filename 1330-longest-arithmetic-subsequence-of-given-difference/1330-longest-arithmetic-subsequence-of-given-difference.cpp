71d18390018ee4167a4d52fba51466f0092b39ee
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-10-28 09:22:36 +0530

    Time: 52 ms (76.47%), Space: 60.5 MB (32.76%) - LeetHub

--- 1330-longest-arithmetic-subsequence-of-given-difference/1330-longest-arithmetic-subsequence-of-given-difference.cpp
+++ 1330-longest-arithmetic-subsequence-of-given-difference/1330-longest-arithmetic-subsequence-of-given-difference.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
-    int longestSubsequence(vector<int>& arr, int diff) {
+int call(int i,int prev,vector<int> &arr,int &n,int &diff, vector<vector<int>>&dp)
+{
+    if(i>=n)
+    return 0;
+    if(dp[prev+1][i]!=-1)
+    return dp[prev+1][i];
+    if(prev==-1)
+    {
+        int m1=1+call(i+1,i,arr,n,diff,dp);
+        int m2=call(i+1,prev,arr,n,diff,dp);
+        return dp[prev+1][i]=max(m1,m2);
+    }
+    else if(arr[i]-arr[prev]==diff)
+    {
+        int m1=1+call(i+1,i,arr,n,diff,dp);
+        int m2=call(i+1,prev,arr,n,diff,dp);
+        return dp[prev+1][i]=max(m1,m2);
+    }
+    return call(i+1,prev,arr,n,diff,dp);
+    
+}
+    
+        int longestSubsequence(vector<int>& arr, int diff) {
         unordered_map<int, int> dp; // Stores the longest subsequence ending with each value
         int maxLength = 0;
 
@@ -12,4 +34,5 @@ public:
 
         return maxLength;
     }
+        
 };
