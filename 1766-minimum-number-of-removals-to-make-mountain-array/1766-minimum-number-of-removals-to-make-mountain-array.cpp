32d6d169c7c8436e89da352609d5f47e264867cc
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-10-30 22:23:30 +0530

    Time: 627 ms (6.06%), Space: 285.2 MB (5.11%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-int callinc(vector<int> &nums,int s,int e,int prev,int &n,vector<vector<int>> &dp)
+int callinc(vector<int> &nums,int s,int prev,int &n,vector<vector<int>> &dp)
 {
-    if(prev>=n-1 || s>=n || e<s)
+    if(s>=n || prev>=n-1)
     return 0;
     if(dp[s][prev]!=-1)
     return dp[s][prev];
     int len=0;
     if(nums[s]<nums[prev])
     {
-        len=max(len,1+callinc(nums,s+1,e,s,n,dp));
+        len=max(len,1+callinc(nums,s+1,s,n,dp));
     }
     
-    len=max(len,callinc(nums,s+1,e,prev,n,dp));
+    len=max(len,callinc(nums,s+1,prev,n,dp));
     
     return dp[s][prev]=len;
 }
-int calldec(vector<int> &nums,int s,int e,int aff,int &n,vector<vector<int>> &dp)
+int calldec(vector<int> &nums,int e,int aff,int &n,vector<vector<int>> &dp)
 {
-    if(aff<=0 ||e<0 || e<s)
+    if(e<0 || aff<=0)
     return 0;
     if(dp[e][aff]!=-1)
     return dp[e][aff];
     int len=0;
     if(nums[e]<nums[aff])
     {
-        len=max(len,1+calldec(nums,s,e-1,e,n,dp));
+        len=max(len,1+calldec(nums,e-1,e,n,dp));
     }
-    len=max(len,calldec(nums,s,e-1,aff,n,dp));
+    len=max(len,calldec(nums,e-1,aff,n,dp));
     
     return dp[e][aff]=len;
 }
@@ -40,10 +40,10 @@ int calldec(vector<int> &nums,int s,int e,int aff,int &n,vector<vector<int>> &dp
         for(int i=1;i<n-1;i++)
         {
             
-            int leftlen=calldec(nums,0,i-1,i,n,dp1);
-            int rightlen=callinc(nums,i+1,n-1,i,n,dp2);
+            int leftlen=calldec(nums,i-1,i,n,dp1);
+            int rightlen=callinc(nums,i+1,i,n,dp2);
 
-            if(leftlen!=0 && rightlen!=0)
+            if(leftlen>0 && rightlen>0)
             del=min(n-leftlen-1-rightlen,del);
 
         }
