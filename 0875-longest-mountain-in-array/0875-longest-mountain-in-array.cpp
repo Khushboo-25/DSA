0d3306b198568286a713adfdf928b2268c0a9224
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-10-30 23:58:32 +0530

    Time: 0 ms (100%), Space: 23.1 MB (7.15%) - LeetHub

--- 0875-longest-mountain-in-array/0875-longest-mountain-in-array.cpp
+++ 0875-longest-mountain-in-array/0875-longest-mountain-in-array.cpp
@@ -1,28 +1,47 @@
 class Solution {
 public:
-    int longestMountain(vector<int>& nums) {
-        int n = nums.size();
-        if (n<3) return 0;
+int callinc(vector<int> &nums,int prev,int e,int &n,vector<int> &dp)
+{
+    if(e>=n || prev>=n-1)
+    return 0;
+    if(dp[prev]!=-1)
+    return dp[prev];
+    int len=0;
+    if(nums[e]<nums[prev])
+    {
+        return dp[prev]=1+callinc(nums,e,e+1,n,dp);
+    }
+    return dp[prev]=0;
+}
+int calldec(vector<int> &nums,int s,int aff,int &n,vector<int> &dp)
+{
+    if(s<0 ||aff<=0)
+    return 0;
+    if(dp[aff]!=-1)
+    return dp[aff];
+    int len=0;
+    if(nums[s]<nums[aff])
+    {
+        return dp[aff]=1+calldec(nums,s-1,s,n,dp);
+    }
+    return dp[aff]=0;
+}
+    int longestMountain(vector<int>& nums) 
+    {
+         int n=nums.size();
+        int ans=0;
+        vector<int> dp1(n,-1);
+        vector<int> dp2(n,-1);
+        for(int i=1;i<n-1;i++)
+        {
+            
+            int leftlen=calldec(nums,i-1,i,n,dp1);
+            int rightlen=callinc(nums,i,i+1,n,dp2);
 
-        int max_length = 0;
+            if(leftlen>0 && rightlen>0)
+            ans=max(ans,leftlen+1+rightlen);
 
-        for (int i = 1; i < n - 1; i++) {
-            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
-                int left = i, right = i;
-
-                while (left > 0 && nums[left - 1] < nums[left]) {
-                    left--;
-                }
-
-                while (right < n - 1 && nums[right + 1] < nums[right]) {
-                    right++;
-                }
-
-                int length = right - left + 1;
-                max_length = max(max_length, length);
-            }
         }
-        
-        return max_length;
+        return ans;
     }
 };
