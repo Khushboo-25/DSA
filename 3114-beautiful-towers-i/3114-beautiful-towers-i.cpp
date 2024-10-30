43ab8bf7822340cbb9bf0744c5a5f6d30e569ee7
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-10-31 00:48:52 +0530

    Time: 91 ms (6.52%), Space: 28.9 MB (46.14%) - LeetHub

--- 3114-beautiful-towers-i/3114-beautiful-towers-i.cpp
+++ 3114-beautiful-towers-i/3114-beautiful-towers-i.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
-long long calldsc(int s,int aff,vector<int> &a)
+
+long long calldsc(int s,int aff,vector<int> &a,vector<long long>&dp)
 {
     if(s<0)
     return 0;
     if(a[s]<a[aff])
     {
-        return a[s]+calldsc(s-1,s,a);
+        return dp[aff]=a[s]+calldsc(s-1,s,a,dp);
     }
     else
     {
-        return a[aff]+calldsc(s-1,aff,a);
+        return dp[aff]=a[aff]+calldsc(s-1,aff,a,dp);
     }
+    
 }
-long long callinc(int e,int &n,int prev,vector<int>&a)
+long long callinc(int e,int &n,int prev,vector<int>&a,vector<long long> &dp)
 {
     if(e>=n)
     return 0;
     if(a[e]<a[prev])
-    return a[e]+callinc(e+1,n,e,a);
-    return a[prev]+callinc(e+1,n,prev,a);
+    return dp[prev]=a[e]+callinc(e+1,n,e,a,dp);
+    return dp[prev]=a[prev]+callinc(e+1,n,prev,a,dp);
 }
     long long maximumSumOfHeights(vector<int>& a) {
-        long long ans=0;
+         long long ans=0;
         int n=a.size();
+        vector<long long>dp1(n,-1);
+        vector<long long>dp2(n,-1);
         for(int i=0;i<n;i++)
         {
-            long long sumleft=calldsc(i-1,i,a);
-            long long sumright=callinc(i+1,n,i,a);
+            long long sumleft=calldsc(i-1,i,a,dp1);
+            long long sumright=callinc(i+1,n,i,a,dp2);
             ans=max(ans,sumleft+sumright+a[i]);
 
         }
