a66daa854a045c50b94c074b68e15e439b0beace
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-11-12 15:52:48 +0530

    Time: 171 ms (71.85%), Space: 146.3 MB (25.23%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
@@ -9,18 +9,14 @@ public:
         int n = nums.size();
 
         for (int i = 0; i < n; i++) {
-            // Check for almost duplicate within the current window
             auto it = st.lower_bound(nums[i] - valueDiff);
             
-            // Check if this element is within `valueDiff`
             if (it != st.end() && abs(*it - nums[i]) <= valueDiff) {
                 return true;
             }
             
-            // Insert the current element into the set
             st.insert(nums[i]);
 
-            // Maintain the size of the set to be `indexDiff`
             if (i >= indexDiff) {
                 st.erase(st.find(nums[i - indexDiff]));
             }
