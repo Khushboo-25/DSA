d61e5cf5e9dddef4d509ccdac58a4589094a823e
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-11-21 11:54:45 +0530

    Time: 60 ms (14.62%), Space: 58.3 MB (9.49%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -1,56 +1,85 @@
 class Solution {
 public:
-int dfs(int i,vector<int> &vv,vector<vector<pair<int,int>>> &adj)
-{
-    // vv[i]=1;
-    int sm=0;
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-    pq.push({0,i});
-    while(!pq.empty())
+void update(vector<int> &vv,int &mst,vector<vector<int>> &adj,int i)
+  {
+      priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+      pq.push({0,i});
+      while(!pq.empty())
+      {
+          auto it=pq.top();
+          int d=it.first;
+          int node=it.second;
+          pq.pop();
+          if(vv[node]==0)
+          {
+              vv[node]=1;
+              mst+=d;
+              for(auto it: adj[node])
+              {
+                  if(vv[it]==0)
+                  {
+                    //   vv[it]=d+1;
+                      pq.push({1,it});
+                  }
+              }
+          }
+      }
+  }
+    int Solve(int n, vector<vector<int>>& edge) 
     {
-        auto it=pq.top();
-        int w=it.first;
-        int node=it.second;
-        pq.pop();
-        if(vv[node]==0)
-        {
-            sm+=w;
-            vv[node]=1;
-            for(auto it1:adj[node])
-            {
-                int nw=it1.first;
-                int w2=it1.second;
-                if(vv[nw]==0)
-                pq.push({w2,nw});
-            }
-        }
-    }
-    return sm;
-}
-    int makeConnected(int n, vector<vector<int>>& conn) 
-    {
-        vector<vector<pair<int,int>>> adj(n);
-        int sum=0;
-        for(auto it: conn)
+        int med=edge.size();
+        int cn=0;
+        int mst=0;
+        vector<int> vv(n,0);
+        vector<vector<int> > adj(n);
+        for(auto it:edge)
         {
             int x=it[0];
             int y=it[1];
-            sum+=1;
-            adj[x].push_back({y,1});
-            adj[y].push_back({x,1});
-
+            adj[x].push_back(y);
+            adj[y].push_back(x);
         }
-        if(sum<n-1)
-        return -1;
-        int tm=n-1;
-        vector<int> vv(n,0);
-        // int cn=0;
         for(int i=0;i<n;i++)
         {
             if(vv[i]==0)
-            tm-=dfs(i,vv,adj);
+            {
+                cn++;
+                update(vv,mst,adj,i);
+            }
         }
-        return tm;
-        
+        if(med-mst>=cn-1)
+        {
+            return cn-1;
+        }
+        return -1;
+        // code here
+    }
+    int makeConnected(int n, vector<vector<int>>& edge) 
+    {
+        int med=edge.size();
+        int cn=0;
+        int mst=0;
+        vector<int> vv(n,0);
+        vector<vector<int> > adj(n);
+        for(auto it:edge)
+        {
+            int x=it[0];
+            int y=it[1];
+            adj[x].push_back(y);
+            adj[y].push_back(x);
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(vv[i]==0)
+            {
+                cn++;
+                update(vv,mst,adj,i);
+            }
+        }
+        if(med-mst>=cn-1)
+        {
+            return cn-1;
+        }
+        return -1;
     }
 };
