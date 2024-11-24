b4376242487fb23429f015032d2d998a6dcbee1e
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-11-24 16:05:16 +0530

    Time: 56 ms (42.08%), Space: 20.1 MB (12.9%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -1,45 +1,52 @@
 class Solution {
 public:
-int call (int i1,int j1,int j2,vector<vector<int> > &grid,int &n,int &m,vector<vector<vector<int>>>&dp)
-  {
-      if(j1<=0 || j1>m ||j2<=0 ||j2>m)
-      {
-            return 0;
-      }
-      if(dp[i1][j1][j2]!=-1)
-      return dp[i1][j1][j2];
-      if(i1==n)
-      {
-          if(j1==j2)
-          return grid[i1-1][j1-1];
-          else
-          return grid[i1-1][j1-1]+grid[i1-1][j2-1];
-      }
-      
-      int mx=INT_MIN;
-      for(int d1=-1;d1<=1;d1++)
-      {
-          for(int d2=-1;d2<=1;d2++)
-          {
-              int v=0;
-              
-              if(j1==j2)
-              v=grid[i1-1][j1-1];
-              else
-              v=grid[i1-1][j1-1]+grid[i1-1][j2-1];
-              v+=call(i1+1,j1+d1,j2+d2,grid,n,m,dp);
-              mx=max(v,mx);
-          }
-      }
-      return dp[i1][j1][j2]=mx;
-      
-      
-      
-  }
-    int cherryPickup(vector<vector<int>>& grid) {
+int call(int &n,int &m,int i,int j1,int j2,vector<vector<int>> &grid,vector<vector<vector<int>>>&dp)
+{
+    if(j1>=m || j1<0 || j2>=m || j2<0)
+    return 0;
+
+    if(dp[i][j1][j2]!=-1)
+    return dp[i][j1][j2];
+    if(i==n-1)
+    {
+        if(j1==j2)
+        {
+           return grid[i][j1];
+
+        }
+        else
+        {
+            return grid[i][j1]+grid[i][j2];
+        }
+    }
+    int ans=0;
+    for(int d1=-1;d1<=1;d1++)
+    {
+        for(int d2=-1;d2<=1;d2++)
+        {
+            int v=0;
+            if(j1==j2)
+            {
+                v+=grid[i][j1];
+                
+            }   
+            else
+            v+=grid[i][j1]+grid[i][j2];
+            v+=call(n,m,i+1,j1+d1,j2+d2,grid,dp);
+            ans=max(ans,v);
+
+        }
+    }
+    return dp[i][j1][j2]=ans;
+    
+
+}
+    int cherryPickup(vector<vector<int>>& grid) 
+    {
         int n=grid.size(),m=grid[0].size();
-        int i1=1,j1=1,j2=m;
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(m + 1, vector<int>(m + 1, -1)));
-        return call(i1,j1,j2,grid,n,m,dp);
+        int i=0,j1=0,j2=m-1;
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(m+1,vector<int>(m+1,-1)));
+        return call(n,m,i,j1,j2,grid,dp);
+        
     }
 };
