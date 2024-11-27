f6d0783c4cc8eef39c2534aa8e773ac743bc3c92
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-11-28 00:46:55 +0530

    Time: 3 ms (48.02%), Space: 11 MB (12.26%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)
+        return false;
+        string s1=to_string(x);
+        string s2=s1;
+        reverse(s2.begin(),s2.end());
+        return s1==s2;
+        
+    }
+};
