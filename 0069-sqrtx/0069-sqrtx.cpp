a8e82a07fee0c2a2915d5218b10fe71775b86f2e
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-05-11 11:08:35 +0530

    Time: 0 ms (100%), Space: 8.7 MB (14.96%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,32 +1,9 @@
 class Solution {
 public:
-bool check(int m,int &x)
-{
-    
-    if( m*m<=x)
-    return 1;
-    return 0;
-}
     int mySqrt(int x) 
     {
         return sqrt(x);
-        int s=0,e=x;
-        int ans=x;
-        while(s<=e)
-        {
-            int m=s+(e-s)/2;
-            if(check(m,x))
-            {
-                
-                e=m-1;
-            }
-            else
-            {
-                ans=m;
-                s=m+1;
-            }
-        }
-        return ans;
+        
         
     }
 };
