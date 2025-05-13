b4b2170615d781a56a3af905c8ac27461d5db418
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-05-13 17:40:45 +0530

    Time: 10 ms (3.96%), Space: 33.6 MB (3.27%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -3,19 +3,17 @@ public:
 int mod=1e9+7;
     int trailingZeroes(int n) 
     {
-        vector<int> count;
+        vector<int> count(n+1,0);
         count.push_back(0);
         int n5=0;
-        for(int i=1;i<=n;i++)
+        for(int i=5;i<=n;i+=5)
         {
             
             
             int ans5=0;
-            if(i%5==0)
-            {
+            
                 ans5=(ans5+count[i/5]%mod)%mod+1;
-            }
-            count.push_back(ans5);
+            count[i]=ans5;
             
             n5=n5+count[i];
             // cout<<ans2<<" "<<ans5<<endl;
