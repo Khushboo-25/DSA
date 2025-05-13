dd988c29d13278b333a698d5bf7842d38a01515c
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-05-13 17:31:58 +0530

    Time: 31 ms (3.96%), Space: 65 MB (3.27%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -5,6 +5,7 @@ int mod=1e9+7;
     {
         vector<pair<int,int>> count;
         count.push_back({0,0});
+        int n2=0,n5=0;
         for(int i=1;i<=n;i++)
         {
             int ans2=0;
@@ -18,15 +19,12 @@ int mod=1e9+7;
                 ans5=(ans5+count[i/5].second%mod)%mod+1;
             }
             count.push_back({ans2,ans5});
-            // cout<<ans2<<" "<<ans5<<endl;
-        }
-        int n2=0,n5=0;
-        for(int i=0;i<=n;i++)
-        {
             n2=n2+count[i].first;
             n5=n5+count[i].second;
-
+            // cout<<ans2<<" "<<ans5<<endl;
         }
+        
+        
         return min(n2,n5);
         
     }
