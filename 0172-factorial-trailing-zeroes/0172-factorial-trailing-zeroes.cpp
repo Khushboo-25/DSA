63587b0b368e9442826ec175602feb342c73c74b
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-05-13 17:36:33 +0530

    Time: 0 ms (100%), Space: 8 MB (85.47%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -3,44 +3,44 @@ public:
 int mod=1e9+7;
     int trailingZeroes(int n) 
     {
-        vector<pair<int,int>> count;
-        count.push_back({0,0});
-        int n2=0,n5=0;
-        for(int i=1;i<=n;i++)
-        {
-            if(i%2!=0 && i%5!=0)
-            {
-                count.push_back({0,0});
-                continue;
-            }
+        // vector<pair<int,int>> count;
+        // count.push_back({0,0});
+        // int n2=0,n5=0;
+        // for(int i=1;i<=n;i++)
+        // {
+        //     if(i%2!=0 && i%5!=0)
+        //     {
+        //         count.push_back({0,0});
+        //         continue;
+        //     }
 
-            int ans2=0;
-            if(i%2==0)
-            {
-                ans2=(ans2+count[i/2].first%mod)%mod+1;
-            }
-            int ans5=0;
-            if(i%5==0)
-            {
-                ans5=(ans5+count[i/5].second%mod)%mod+1;
-            }
-            count.push_back({ans2,ans5});
-            n2=n2+count[i].first;
-            n5=n5+count[i].second;
-            // cout<<ans2<<" "<<ans5<<endl;
-        }
-        
-        
-        return min(n2,n5);
-
-
-
-        // int count = 0;
-        // while (n > 0) {
-        //     n /= 5;
-        //     count += n;
+        //     int ans2=0;
+        //     if(i%2==0)
+        //     {
+        //         ans2=(ans2+count[i/2].first%mod)%mod+1;
+        //     }
+        //     int ans5=0;
+        //     if(i%5==0)
+        //     {
+        //         ans5=(ans5+count[i/5].second%mod)%mod+1;
+        //     }
+        //     count.push_back({ans2,ans5});
+        //     n2=n2+count[i].first;
+        //     n5=n5+count[i].second;
+        //     // cout<<ans2<<" "<<ans5<<endl;
         // }
-        // return count;
+        
+        
+        // return min(n2,n5);
+
+
+
+        int count = 0;
+        while (n > 0) {
+            n /= 5;
+            count += n;
+        }
+        return count;
 
         
     }
