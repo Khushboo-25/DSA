b90502f26deabd59b1247a9990459a3b4c5a9374
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-06-16 21:22:32 +0530

    Time: 0 ms (100%), Space: 7.6 MB (94.41%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
@@ -3,7 +3,7 @@ public:
     bool call(int i, int j, int x, int y, int tt, vector<vector<int>> &dp) {
         if (i + j == tt) return true;
         if (dp[i][j] != -1) return dp[i][j];
-
+       
         dp[i][j] = 0; // mark as visited to prevent infinite recursion
 
         // Fill jug x
@@ -25,7 +25,10 @@ public:
     }
 
     bool canMeasureWater(int x, int y, int tt) {
-        if (tt > x + y) return false; // can't measure more than total capacity
+        
+
+        if (tt > x + y) return false;
+        return tt % __gcd(x, y) == 0; // can't measure more than total capacity
         vector<vector<int>> dp(x + 1, vector<int>(y + 1, -1));
         return call(0, 0, x, y, tt, dp);
     }
