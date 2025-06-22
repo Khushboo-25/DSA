7e4c47ce3e46bec0db57563c7efe7aa44b2b1f4c
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-06-22 23:17:45 +0530

    Time: 11 ms (65.14%), Space: 12.6 MB (75.96%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -14,7 +14,7 @@ public:
         // Build dp and prev arrays
         for (int i = 1; i < n; ++i) {
             for (int j = 0; j < i; ++j) {
-                if (nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]) {
+                if (nums[i] % nums[j] == 0 && dp[j] + 1 >=dp[i]) {
                     dp[i] = dp[j] + 1;
                     prev[i] = j;
                 }
