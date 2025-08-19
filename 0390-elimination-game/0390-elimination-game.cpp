3d882a017efd8538e537327d62b2c7f90052915f
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-08-19 19:07:20 +0530

    Time: 0 ms (100%), Space: 9.7 MB (45.05%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.cpp
+++ 0390-elimination-game/0390-elimination-game.cpp
@@ -1,33 +1,6 @@
 class Solution {
 public:
-    int lastRemaining(int n) 
-    {
-        if(n==1)
-        return 1;
-        if(n==2)
-        {
-            return 2;
-        }
-        if(n==1e9)
-        return 534765398;
-        vector<int>pp;
-        for(int i=2;i<=n;i+=2)
-        {
-            pp.push_back(i);
-        }
-        // int cn=n/2;
-        // int d=-1;
-        while(pp.size()>1)
-        {
-            vector<int>a;
-            int n1=pp.size()-1;
-            for(int i=n1-1;i>=0;i-=2)
-            {
-                a.push_back(pp[i]);
-            }
-            pp=a;
-        }
-        return pp[0];
-
-    }
+    int lastRemaining(int n) {
+    return n == 1 ? 1 : 2 * (1 + n / 2 - lastRemaining(n / 2));
+}
 };
