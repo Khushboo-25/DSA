a9c12ddbfc07cbbb6ffbd165b3c60bba9dfdf44f
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-08-20 17:56:37 +0530

    Time: 463 ms (10.6%), Space: 288.7 MB (5.02%) - LeetHub

--- 3823-count-islands-with-total-value-divisible-by-k/3823-count-islands-with-total-value-divisible-by-k.cpp
+++ 3823-count-islands-with-total-value-divisible-by-k/3823-count-islands-with-total-value-divisible-by-k.cpp
@@ -1,42 +1,52 @@
-
-typedef long long ll;
 class Solution {
 public:
-    int countIslands(vector<vector<int>>& grid, int k) {
-        int m = grid.size(), n = grid[0].size();
-    vector<vector<bool>> visited(m, vector<bool>(n, false));
-    int count = 0;
+vector<vector<int>>dr={{0,-1},{-1,0},{0,1},{1,0}};
+int bfs(int i,int j,vector<vector<int>>&vv,vector<vector<int>>&grid,int &n,int&m,int &k)
+{
+    int s=0;
+    queue<pair<int,int>>pq;
+    pq.push({i,j});
 
-    vector<pair<int,int>> directions = {{0,1}, {0,-1}, {1,0}, {-1,0}};
-
-    for(int i = 0; i < m; ++i) {
-        for(int j = 0; j < n; ++j) {
-            if(grid[i][j] > 0 && !visited[i][j]) {
-                // start DFS
-                stack<pair<int,int>> stk;
-                stk.push({i,j});
-                visited[i][j] = true;
-                ll sum = 0;
-
-                while(!stk.empty()) {
-                    auto [x, y] = stk.top(); stk.pop();
-                    sum += grid[x][y];
-
-                    for(auto [dx, dy] : directions) {
-                        int nx = x + dx, ny = y + dy;
-                        if(nx >= 0 && ny >= 0 && nx < m && ny < n &&
-                           grid[nx][ny] > 0 && !visited[nx][ny]) {
-                            visited[nx][ny] = true;
-                            stk.push({nx, ny});
-                        }
-                    }
-                }
-
-                if(sum % k == 0) ++count;
+    vv[i][j]=1;
+    while(!pq.empty())
+    {
+        int x=pq.front().first;
+        int y=pq.front().second;
+        s=(s+grid[x][y])%k;
+        pq.pop();
+        for(auto it: dr)
+        {
+            int x1=x+it[0];
+            int y1=y+it[1];
+            if(x1<n && x1>=0 && y1<m && y1>=0 &&vv[x1][y1]==0 && grid[x1][y1]!=0)
+            {
+                pq.push({x1,y1});
+                vv[x1][y1]=1;
             }
         }
     }
-
-    return count;
+    return s;
+}
+    int countIslands(vector<vector<int>>& grid, int k) 
+    {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vv(n,vector<int>(m,0));
+        int cn=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(vv[i][j]==0 && grid[i][j]!=0)
+                {
+                    int s=bfs(i,j,vv,grid,n,m,k);
+                    if(s%k==0)
+                    {
+                        cn++;
+                    }
+                }
+            }
+        }
+        return cn;
     }
 };
