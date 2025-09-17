9d32b3c5271543ebdb8151bae07dc0c19891e778
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-09-17 20:18:10 +0530

    Time: 85 ms (55.95%), Space: 48.1 MB (20.39%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -3,43 +3,41 @@ public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) 
     {
         vector<vector<pair<int,int>>>adj(n+1);
-        for(auto it:times)
+        for(auto it: times)
         {
             int x=it[0];
             int y=it[1];
-            int tm=it[2];
-            adj[x].push_back({y,tm});
+            int w=it[2];
+            adj[x].push_back({y,w});
         }
-        vector<int>tt(n+1,INT_MAX);
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({0,k});
-        tt[k]=0;
-        while(!pq.empty())
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pp;
+        vector<int>vv(n+1,INT_MAX);
+        vv[k]=0;
+        pp.push({0,k});
+        while(!pp.empty())
         {
-            auto tp=pq.top();
-            int node=tp.second;
-            int d=tp.first;
-            pq.pop();
-            for(auto it:adj[node])
+            auto tp=pp.top();
+            pp.pop();
+            int w1=tp.first;
+            int x1=tp.second;
+            for(auto it:adj[x1])
             {
-                int nw=it.first;
+                int x2=it.first;
                 int d2=it.second;
-                if(tt[nw]>d+d2)
+                if(d2+w1<vv[x2])
                 {
-                    tt[nw]=d+d2;
-                    pq.push({tt[nw],nw});
+                    vv[x2]=w1+d2;
+                    pp.push({vv[x2],x2});
                 }
             }
         }
         int ans=0;
         for(int i=1;i<=n;i++)
         {
-            ans=max(tt[i],ans);
+            ans=max(ans,vv[i]);
         }
         if(ans==INT_MAX)
         return -1;
         return ans;
-
-        
     }
 };
