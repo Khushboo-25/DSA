804259709ede00a1d74fbc994e36366355a02a8c
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-09-17 18:09:56 +0530

    Time: 93 ms (6.74%), Space: 54 MB (7.32%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-// int MOD=1e9+7;
-int call(int i,int csum,int &sum,int &n,vector<int> &a,vector<vector<int>>&dp)
+int call(int i,int tt,vector<int> &c,int &n,vector<vector<int>>&dp)
 {
-    if(csum==sum)
-    return 0;
-    
-    if(i>=n || csum>sum)
-    return INT_MAX;
-    
-    if(dp[i][csum]!=-1)
-    return dp[i][csum];
-    int ways=INT_MAX;
-    if(a[i]<=sum-csum)
+    if(i>=n)
     {
-        int take=call(i,csum+a[i],sum,n,a,dp);
-        if(take!=INT_MAX)
-        ways=min(take+1,ways);
+        if(tt==0)
+        {
+            return 0;
+        }
+        return 1e5;
     }
+    if(dp[i][tt]!=-1)
+    return dp[i][tt];
+    int ans=1e5;
+    if(c[i]<=tt)
+    {
+        ans=min(1+call(i+1,tt-c[i],c,n,dp),ans);
+        ans=min(1+call(i,tt-c[i],c,n,dp),ans);
+    }
+    ans=min(ans,call(i+1,tt,c,n,dp));
+    return dp[i][tt]=ans;
 
-    int nottake=call(i+1,csum,sum,n,a,dp);
-    ways=min(nottake,ways);
-    return dp[i][csum]=ways;
 }
-    int coinChange(vector<int>& a, int sum) {
-        int n=a.size();
-        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-        int ways=call(0,0,sum,n,a,dp);
+    int coinChange(vector<int>& coins, int amount) 
+    {
+        int n=coins.size();
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+        int ans=call(0,amount,coins,n,dp);
+        if(ans>1e4)
+        return -1;
+        return ans;
         
-        return ways==INT_MAX?-1:ways;
     }
 };
