b51a3a401ef878df9edc1f0126e608978c981f51
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-09-27 01:53:13 +0530

    Time: 13 ms (29.01%), Space: 14.3 MB (91.03%) - LeetHub

---
+++ 0774-maximum-depth-of-n-ary-tree/0774-maximum-depth-of-n-ary-tree.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+void call(Node* root, int &ans,int n1)
+{
+    if(root==NULL)
+    {
+        cout<<n1<<" ";
+        
+        return;
+    }
+    ans=max(1+n1,ans);
+    for(auto it: root->children)
+    {
+        
+        call(it,ans,1+n1);
+    }
+}
+    int maxDepth(Node* root) 
+    {
+        int ans=0;
+        call(root,ans,0);
+        return ans;
+    }
+};
