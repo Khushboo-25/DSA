0af529866229dc43900b8f8f2a469f861463dcf2
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-10-06 20:39:31 +0530

    Time: 8 ms (50.24%), Space: 12.8 MB (86.05%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,78 +1,36 @@
-#define point pair<int,pair<int,int>>
-vector<pair<int,int>> dr={{-1,0},{1,0},{0,-1},{0,1}};
 class Solution {
 public:
+typedef pair<int,pair<int,int>> ppl;
+vector<pair<int,int>>dr={{-1,0},{0,-1},{1,0},{0,1}};
     int swimInWater(vector<vector<int>>& grid) 
     {
-        int n=grid.size(),m=grid[0].size();
-        vector<vector<int>>vv(n,vector<int>(m,INT_MAX));
+        int n=grid.size();
+        vector<vector<int>>vv(n,vector<int>(n,INT_MAX));
+        priority_queue<ppl,vector<ppl>,greater<ppl>>pq;
+        pq.push({grid[0][0],{0,0}});
         
-        // if(!(grid[0][0]<=0))
-        // return -1;
-        priority_queue<point,vector<point>,greater<point>>pq;
-        
-
         vv[0][0]=grid[0][0];
-        pq.push({vv[0][0],{0,0}});
         while(!pq.empty())
         {
-            auto it=pq.top();
-            int tm=it.first;
-            int x=it.second.first;
-            int y=it.second.second;
+            int tm=pq.top().first;
+            int x=pq.top().second.first;
+            int y=pq.top().second.second;
             pq.pop();
-            // bool check=0;
-            for( auto it1: dr)
+            for(auto it: dr)
             {
-                int x1=x+it1.first;
-                int y1=y+it1.second;
-                if(x1<n && x1>=0 && y1<m && y1>=0 )
+                int x1=x+it.first;
+                int y1=y+it.second;
+                if(x1>=0 && x1<n && y1<n && y1>=0 )
                 {
-                    if(grid[x1][y1]<=tm)
+                    int tm1=max(tm,grid[x1][y1]);
+                    if(tm1<vv[x1][y1])
                     {
-                        if(vv[x1][y1]>tm)
-                        {
-                        vv[x1][y1]=tm;
-                        pq.push({vv[x1][y1],{x1,y1}});
-                        }
-                        
+                        vv[x1][y1]=tm1;
+                        pq.push({tm1,{x1,y1}});
                     }
-                    else
-                    {
-                        if(vv[x1][y1]>grid[x1][y1])
-                        {
-                            vv[x1][y1]=grid[x1][y1];
-                            pq.push({vv[x1][y1],{x1,y1}});
-                        }
-                        
-                    }
-                    
                 }
             }
-            // if(check)
-            // {
-            //     for( auto it: dr)
-            //     {
-            //         int x1=x+it.first;
-            //         int y1=y+it.second;
-            //         if(x1<n && x1>=0 && y1<m && y1>=0 )
-            //         {
-            //             if(grid[x1][y1]<=tm )
-            //             {
-            //                 int tm2=tm;
-            //                 if(vv[x1][y1]>tm2)
-            //                 {
-            //                     vv[x1][y1]=tm2;
-            //                     pq.push({vv[x1][y1],{x1,y1}});
-            //                 }
-            //             }
-            //         }
-            //     }
-            // }
         }
-        if(vv[n-1][m-1]==INT_MAX)
-        return -1;
-        return vv[n-1][m-1];
-        
+        return vv[n-1][n-1];
     }
 };
