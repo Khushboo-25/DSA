856c7883c1e61c92e123207d3a9f46f5d5085932
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-10-08 19:41:53 +0530

    Time: 0 ms (100%), Space: 9.1 MB (75.54%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,21 +1,12 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    string decodeString(string s) {
-        int i = 0; // index ref
-        return decode(s, i);
-    }
-
-    string decode(const string& s, int& i) 
+string solve(string &s,int &i)
+{
+    int n=s.size();
+    int num=0;
+    string fs="";
+    while(i<n)
     {
-      string ans="";
-      int n=s.size();
-      int num=0;
-      while(i<n)
-      {
-        
         if(isdigit(s[i]))
         {
             num=num*10+(s[i]-'0');
@@ -24,26 +15,31 @@ public:
         else if(s[i]=='[')
         {
             i++;
-            string ans1=decode(s,i);
+            string ans1=solve(s,i);
             for(int j=0;j<num;j++)
             {
-                ans+=ans1;
+                fs+=ans1;
             }
             num=0;
         }
         else if(s[i]==']')
         {
             i++;
-            return ans;
+            return fs;
         }
         else
         {
-            
-            ans+=s[i];
+            fs+=s[i];
             i++;
         }
-      }
+    }
+    return fs;
 
-      return ans;
+}
+    string decodeString(string s) 
+    {
+        int i=0;
+        return solve(s,i);
+        
     }
 };
