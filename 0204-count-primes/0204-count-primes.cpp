19a4f15dbee7d7d0342e0e73c770487d2768b6f7
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-10-09 22:21:51 +0530

    Time: 135 ms (92.42%), Space: 132.6 MB (8.42%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        vector<int>vv(n,1);// prime
+        vector<int>vv(n+1,1);// prime
         if(n<=1)
         return 0;
         vv[0]=0;
         
         vv[1]=0;
-        for(int i=2;i*i<n;i++)
+        for(int i=2;i*i<=n;i++)
         {
             if(vv[i]==1)
             {
-                for(int j=i*i;j<n;j+=i)
+                for(int j=i*i;j<=n;j+=i)
                 {
                     vv[j]=0;
                 }
