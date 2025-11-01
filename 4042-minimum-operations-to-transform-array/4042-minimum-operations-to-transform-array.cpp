c15686e0591ecc32b095975eb1ace3e2023968df
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-11-01 13:26:29 +0530

    Time: 3294 ms (5.07%), Space: 213.9 MB (71.16%) - LeetHub

---
+++ 4042-minimum-operations-to-transform-array/4042-minimum-operations-to-transform-array.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    long long minOperations(vector<int>& nums1, vector<int>& nums2) 
+    {
+        long long ans=0;
+        int n=nums1.size();
+        int p=nums2[n];
+        
+        int add=INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            int d=abs(nums1[i]-nums2[i]);
+            ans+=d;
+            cout<<d<<" ";
+        }
+        cout<<endl;
+        for(int i=0;i<n;i++)
+        {
+            if(nums1[i]<=nums2[i])     
+            {
+                if(p<=nums1[i])    /// p n1 n2
+                {
+                    int dp=abs(nums1[i]-p);
+                    cout<<dp+1<<" ";
+                    add=min(add,dp+1);
+                }
+                else if(p>nums2[i])   // n1 n2 p
+                {
+                    int dn=abs(nums2[i]-p);
+                    cout<<dn+1<<" ";
+                    add=min(add,dn+1);
+                }
+                else///n1 p n2
+                {
+                    cout<<1<<" ";
+                    add=min(add,1);
+                }
+            }
+            else
+            {
+                if(p<=nums2[i])    // p n2 n1 
+                {
+                    int dp=abs(nums2[i]-p);
+                    cout<<dp+1<<" ";
+                    add=min(add,dp+1);
+                }
+                else if(p>nums1[i]) // n2 n1 p
+                {
+                    int dn=abs(nums1[i]-p);
+                    cout<<dn+1<<" ";
+                    add=min(add,dn+1);
+                }
+                else //n2 p n1
+                {
+                    cout<<1<<" ";
+                    add=min(add,1);
+                }
+            }
+        }
+    
+        return ans+add;
+        
+    }
+};
