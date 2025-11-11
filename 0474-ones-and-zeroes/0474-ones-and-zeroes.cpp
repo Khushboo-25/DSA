f5582ddf5f45127e30194cad5db94d22832ed19c
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-11-11 20:55:30 +0530

    Time: 146 ms (54.01%), Space: 107 MB (15.26%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-int call(int i,int m0,int m1,int &n,int &n0,int &n1,vector<pair<int,int>>&st,unordered_map<string,int>&pp)
+int call(int i,int m0,int m1,int &n,int &n0,int &n1,vector<pair<int,int>>&st,vector<vector<vector<int>>>&dp)
 {
     if(i>=n)
     return 0;
     int ans=0;
-    string fs=to_string(i)+","+to_string(m0)+","+to_string(m1);
-    if(pp.find(fs)!=pp.end())
-    return pp[fs];
-    ans=max(ans,call(i+1,m0,m1,n,n0,n1,st,pp));
+    if(dp[i][m0][m1]!=-1)
+    return dp[i][m0][m1];
+    ans=max(ans,call(i+1,m0,m1,n,n0,n1,st,dp));
     if((m0+st[i].first)<=n0 && (m1+st[i].second)<=n1)
     {
-        ans=max(ans,1+call(i+1,m0+st[i].first,m1+st[i].second,n,n0,n1,st,pp));
+        ans=max(ans,1+call(i+1,m0+st[i].first,m1+st[i].second,n,n0,n1,st,dp));
     }
-    return pp[fs]=ans;
+    return dp[i][m0][m1]=ans;
 }
     int findMaxForm(vector<string>& str, int &n0, int &n1) 
     {
@@ -32,7 +31,7 @@ int call(int i,int m0,int m1,int &n,int &n0,int &n1,vector<pair<int,int>>&st,uno
             }
             st.push_back({m0,m1});
         }
-        unordered_map<string,int>pp;
-        return call(0,0,0,n,n0,n1,st,pp);
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(n0+1,vector<int>(n1+1,-1)));
+        return call(0,0,0,n,n0,n1,st,dp);
     }
 };
