569eca5444c68cabf4ed88b3b90f6c178a9dccea
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-11-23 21:45:03 +0530

    Time: 315 ms (14.98%), Space: 166.5 MB (14.01%) - LeetHub

---
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+vector<pair<int,int>>dr={{-1,0},{0,-1}};
+int mod=1e9+7;
+int call(int i,int j,int rm,int &n,int &m,vector<vector<int>>&grid,int &k,vector<vector<vector<int>>>&dp)
+{
+    if(i==0 && j==0)
+    {
+        if(rm==0)
+        return 1;
+        return 0;
+    }
+    if(i<0 || j<0)
+    return 0;
+    if(dp[i][j][rm]!=-1)
+    return dp[i][j][rm];
+    int ans=0;
+    for(auto it: dr)
+    {
+        int x=i+it.first;
+        int y=j+it.second;
+        if(x>=0 && y>=0)
+        {
+            int rm1=(rm+grid[x][y])%k;
+            ans=(ans+call(x,y,rm1,n,m,grid,k,dp))%mod;
+        }
+    }
+    return dp[i][j][rm]=ans;
+}
+    int numberOfPaths(vector<vector<int>>& grid, int k) 
+    {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(m+1,vector<int>(k+1,-1)));
+        return call(n-1,m-1,(grid[n-1][m-1])%k,n,m,grid,k,dp);
+
+    }
+};
