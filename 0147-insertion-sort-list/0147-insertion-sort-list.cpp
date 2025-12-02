1f845fc362e77f68270f07a422f290971c1da244
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-12-02 19:06:22 +0530

    Time: 3 ms (82.34%), Space: 15 MB (5.45%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.cpp
+++ 0147-insertion-sort-list/0147-insertion-sort-list.cpp
@@ -11,6 +11,27 @@
 class Solution {
 public:
     ListNode* insertionSortList(ListNode* head) {
+        ListNode* root=head;
+        priority_queue<int> q;
+        stack<int> st;
         
+        while(head!=NULL)
+        {
+            q.push(head->val);
+            head=head->next;
+        }
+        head=root;
+        while(!q.empty())
+        {
+            st.push(q.top());
+            q.pop();
+        }
+        while(head!=NULL)
+        {
+            head->val=st.top();
+            st.pop();
+            head=head->next;
+        }
+        return root;
     }
 };
