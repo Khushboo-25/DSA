46d3c42d186889f1bbf091d34ea43309f8060316
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2025-12-22 16:45:01 +0530

    Time: 46 ms (21.7%), Space: 105.8 MB (10.76%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.cpp
+++ 2924-find-champion-ii/2924-find-champion-ii.cpp
@@ -1,53 +1,21 @@
 class Solution {
 public:
-void call(vector<int>&topo,int i,vector<int>&vv,vector<vector<int>>&adj)
-{
-    vv[i]=1;
-    for(auto it:adj[i])
-    {
-        if(vv[it]==0)
-        {
-            call(topo,it,vv,adj);
-        }
-    }
-    topo.push_back(i);
-}
-void call2(int i,vector<int>&vv,vector<vector<int>>&adj)
-{
-    vv[i]=1;
-    for(auto it:adj[i])
-    {
-        if(vv[it]==0)
-        call2(it,vv,adj);
-    }
-}
+
     int findChampion(int n, vector<vector<int>>& edges) 
     {
-        vector<int>topo;
+        vector<int>in(n,0);
         vector<vector<int>>adj(n);
         for(auto it:edges)
         {
-            adj[it[0]].push_back(it[1]);
+            in[it[1]]++;
         }
-        vector<int>vv(n,0);
-        for(int i=0;i<n;i++)
-        {
-            if(vv[i]==0)
-            {
-                call(topo,i,vv,adj);
-            }
-        }
-        for(auto &it:vv)
-        it=0;
+        
         vector<int>ans;
-        for(auto it:topo)
-        cout<<it<<" ";
         for(int i=n-1;i>=0;i--)
         {
-            if(vv[topo[i]]==0)
+            if(in[i]==0)
             {
-                call2(topo[i],vv,adj);
-                ans.push_back(topo[i]);
+                ans.push_back(i);
             }
         }
         if(ans.size()!=1)
