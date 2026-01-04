b0f698ce854188f820a4a4b4151bdae645f877ae
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2026-01-04 19:11:11 +0530

    Time: 2630 ms (8.04%), Space: 11.7 MB (95.7%) - LeetHub

---
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+bool call(vector<int>&ks,int i,int &n,vector<int>&nums,int &k,int &sm)
+{
+    if(i>=n)
+    {
+        int ks0=ks[0];
+        for(int i=1;i<k;i++)
+        {
+            if(ks[i]!=ks0)
+            return 0;
+        }
+        return 1;
+    }
+    for(int j=0;j<k;j++)
+    {
+        if(ks[j]+nums[i]<=sm)
+        {
+            
+        ks[j]+=nums[i];
+        if(ks[j]<=sm && call(ks,i+1,n,nums,k,sm))
+        return 1;
+        ks[j]-=nums[i];
+        if(ks[j]==0)
+        break;
+        }
+    }
+    return 0;
+}
+    bool canPartitionKSubsets(vector<int>&nums,int&k)
+    {
+        int n=nums.size();
+        vector<int>ks(k,0);
+        int sm=0;
+        for(auto it:nums)
+        {
+            sm+=it;
+        }
+        sort(nums.begin(),nums.end());
+        if(sm%k!=0)
+        return 0;
+        sm/=k;
+        return call(ks,0,n,nums,k,sm);
+        
+    }
+};
