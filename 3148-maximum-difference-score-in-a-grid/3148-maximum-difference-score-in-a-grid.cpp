5db929320a96592db7c430c868ec040bfa408d20
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2026-01-26 00:44:55 +0530

    Time: 1841 ms (5.26%), Space: 64.4 MB (13.4%) - LeetHub

---
+++ 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxScore(vector<vector<int>>& grid) 
+    {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>mn(n+1,vector<int>(m+1,INT_MIN));
+        for(int j=m-1;j>=0;j--)
+        {
+            for(int i=n-1;i>=0;i--)
+            {
+                mn[i][j]=max({grid[i][j],mn[i+1][j],mn[i][j+1]});
+            }
+        }
+        int ans=INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i+1<n)
+                {
+                    ans=max(ans,mn[i+1][j]-grid[i][j]);
+                }
+                if(j+1<m)
+                ans=max(ans,mn[i][j+1]-grid[i][j]);
+                cout<<mn[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        // if(ans==0)
+        // return -1;
+        return ans;
+    }
+};
