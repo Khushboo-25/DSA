b5e7c5f1f918bfdda3c257d740e5715d7a498a1c
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2026-02-25 21:48:22 +0530

    Time: 2474 ms (5.01%), Space: 506.4 MB (5.55%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -7,6 +7,7 @@ public:
         sort(nums.begin(),nums.end());
         for(int i=0;i<n-2;i++)
         {
+            
             for(int j=i+1;j<n-1;j++)
             {
                 int tt=0-nums[i]-nums[j];
