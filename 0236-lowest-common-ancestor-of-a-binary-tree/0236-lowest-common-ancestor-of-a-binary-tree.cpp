411c13a71c6314f7a26a5bb26a14b4bd863f900f
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2026-02-25 18:45:00 +0530

    Time: 19 ms (6.98%), Space: 21.4 MB (5.38%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,45 +9,31 @@
  */
 class Solution {
 public:
-void call(TreeNode* p,TreeNode* root ,unordered_map<TreeNode*,TreeNode*>&pp)
+void call(unordered_map<TreeNode*,bool>&fp,unordered_map<TreeNode*,bool>&fq,TreeNode* root,TreeNode* &p,TreeNode*&q,TreeNode* &ans)
 {
     if(root==NULL)
+    return;
+    if(root->val==p->val)
+    fp[root]=1;
+    if(root->val==q->val)
+    fq[root]=1;
+    call(fp,fq,root->left,p,q,ans);
+    call(fp,fq,root->right,p,q,ans);
+    if((root->left && fp.find(root->left)!=fp.end()) || (root->right &&fp.find(root->right)!=fp.end()))
+    fp[root]=1;
+    if((root->left && fq.find(root->left)!=fq.end()) || (root->right &&fq.find(root->right)!=fq.end()))
+    fq[root]=1;
+    if(ans==NULL && fq.find(root)!=fq.end() && fp.find(root)!=fp.end())
     {
-        return;
+        ans=root;
     }
-    pp[root]=p;
-    call(root,root->left,pp);
-    call(root,root->right,pp);
-    
 }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        unordered_map<TreeNode*,TreeNode*>pp;
-        call(NULL,root,pp);
-        unordered_map<TreeNode*,bool>vv;
-        while(p!=NULL || q!=NULL)
-        {
-            
-            if(p!=q)
-            {
-                if(p!=NULL)
-                {
-                    vv[p]=1;
-                    p=pp[p];
-                }
-                if(q!=NULL)
-                {
-                    vv[q]=1;
-                    q=pp[q];
-                }
-                
-                if(vv[p]==1)
-                return p;
-                if(vv[q]==1)
-                return q;
-            }
-            else
-            return p;
-        }
-        return NULL;
+        unordered_map<TreeNode*,bool>fp;
+        unordered_map<TreeNode*,bool>fq;
+        TreeNode* ans=NULL;
+        call(fp,fq,root,p,q,ans);
+        return ans;
+        
     }
 };
