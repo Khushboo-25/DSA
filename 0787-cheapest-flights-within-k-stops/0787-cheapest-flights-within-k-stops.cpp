03ec45d83416b5771c85285d2f9c2463a377768c
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2026-03-23 22:46:30 +0530

    Time: 13 ms (12.93%), Space: 18.8 MB (24.22%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,48 @@
+typedef pair<int,pair<int,int>> pl;
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>&ed, int src, int dst, int k) 
+    {
+        k++;
+        vector<vector<pair<int,int>>>adj(n);
+        for(auto it:ed)
+        {
+            int x=it[0];
+            int y=it[1];
+            int wt=it[2];
+            adj[x].push_back({wt,y});
+        }
+        vector<vector<int>>dis(n+1,vector<int>(k+1,INT_MAX));
+        priority_queue<pl,vector<pl>,greater<pl>>pq;
+        pq.push({0,{0,src}});
+        dis[src][0]=0;
+        while(!pq.empty())
+        {
+            int d=pq.top().first;
+            int kt=pq.top().second.first;
+            int x=pq.top().second.second;
+            pq.pop();
+            if(x==dst)
+            return d;
+            for(auto it:adj[x])
+            {
+                int x1=it.second;
+                int d1=it.first;
+                if(kt+1<=k && dis[x1][kt+1]>d+d1)
+                {
+                    dis[x1][kt+1]=d+d1;
+                    pq.push({d+d1,{kt+1,x1}});
+                }
+            }
+        }
+        int ans=INT_MAX;
+        for(int i=0;i<=k;i++)
+        {
+            ans=min(ans,dis[dst][i]);
+        }
+        if(ans==INT_MAX)
+        return -1;
+        return ans;
+        
+    }
+};
