a53e303c942cd809ef7ad5d3a933334854e3c728
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2026-03-23 14:56:12 +0530

    Time: 46 ms (59.28%), Space: 54.6 MB (15.97%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,46 +1,38 @@
 class Solution {
 public:
-bool call(int i,int j,int &n,int &m,string &s,string &p,vector<vector<int>> &dp)
-{ 
-    if(j>=m && i>=n)
-    return 1;
-
-    else if(j>=m)
-    {   
-        if(s[i]=='*')
-        return  call(i+1,j,n,m,s,p,dp);
-        // else
-        return 0;
-    } 
-    else if(i>=n)
+int call(int i,int j,int &n,int &m,string &s,string &t,vector<vector<int>>&dp)
+{
+    if(j<0)
     {
-        if(p[j]=='*')
-        return call(i,j+1,n,m,s,p,dp);
-        return 0;
+        return i<0;
     }
-    else if(dp[i][j]!=-1)
+    if(i<0)
+    {
+        for(int k=j;k>=0;k--)
+        {
+            if(t[k]!='*')
+            return 0;
+        }
+        return 1;
+    }
+    if(dp[i][j]!=-1)
     return dp[i][j];
-    else if(s[i]=='*' || p[j]=='*')
+    if((s[i]==t[j] ||t[j]=='?') && call(i-1,j-1,n,m,s,t,dp))
+    return dp[i][j]=1;
+    if(t[j]=='*' && (call(i-1,j,n,m,s,t,dp) ||call(i,j-1,n,m,s,t,dp) || call(i-1,j-1,n,m,s,t,dp)))
     {
-        return dp[i][j]=call(i+1,j+1,n,m,s,p,dp)||call(i+1,j,n,m,s,p,dp)|| 
-        call(i,j+1,n,m,s,p,dp);
+        return dp[i][j]=1;
     }
-    
-    else if(s[i]=='?' || p[j]=='?')
-    {
-        return dp[i][j]=call(i+1,j+1,n,m,s,p,dp);
-    }
-    else if(s[i]!=p[j])
     return dp[i][j]=0;
-    else 
-    return dp[i][j]=call(i+1,j+1,n,m,s,p,dp);
-}
-    bool isMatch(string s, string p) 
-    {
-        int n=s.size(),m=p.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return call(0,0,n,m,s,p,dp);
+    
 
+}
+    bool isMatch(string s, string t) 
+    {
+        int n=s.size(),m=t.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        
+        return call(n-1,m-1,n,m,s,t,dp);
         
     }
 };
