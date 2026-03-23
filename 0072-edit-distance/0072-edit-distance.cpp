7ce292f3f077a7cabb82f4c76246a135f4143dc4
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2026-03-23 14:23:55 +0530

    Time: 13 ms (19.96%), Space: 13.4 MB (30.02%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
-int call(int i,int j,int &n,int &m,string &s,string &p,vector<vector<int>>&dp)
+int call(int i,int j,int &n,int&m,string &s,string &t,vector<vector<int>>&dp)
 {
-    if(i>=n && j>=m)
-    return 0;
+    if(j>=m)
+    return (n-i); //rest delete
     if(i>=n)
     {
-        return m-j;
+        return (m-j); // rest insert
     }
-    else if(j>=m)
-    {
-        return n-i;
-    }
-    else if(dp[i][j]!=-1)
+    if(dp[i][j]!=-1)
     return dp[i][j];
-    else if(s[i]!=p[j])
+    int ans=INT_MAX;
+    if(s[i]==t[j])
     {
-        int n1=1+call(i+1,j,n,m,s,p,dp);
-        int n2=1+call(i,j+1,n,m,s,p,dp);
-        int n3=1+call(i+1,j+1,n,m,s,p,dp);
-        return dp[i][j]=min(n1,min(n2,n3));
+        ans=min(ans,call(i+1,j+1,n,m,s,t,dp)); // matched
     }
-    else
-    {
-        return dp[i][j]=call(i+1,j+1,n,m,s,p,dp);
-    }
-    
-
+    ans=min(ans,1+call(i,j+1,n,m,s,t,dp));// inserted
+    ans=min(ans,1+call(i+1,j,n,m,s,t,dp));//delete
+    ans=min(ans,1+call(i+1,j+1,n,m,s,t,dp));// replace
+    return dp[i][j]=ans;
 }
-    int minDistance(string s, string p) 
+    int minDistance(string s, string t) 
     {
-        int n=s.size(),m=p.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return call(0,0,n,m,s,p,dp);
+        int n=s.size(),m=t.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return call(0,0,n,m,s,t,dp);
         
     }
 };
