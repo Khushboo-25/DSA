a5287c49ca13a127288c24a9f3dcefce95af0e29
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2026-03-23 16:13:08 +0530

    Time: 10 ms (71.48%), Space: 85.7 MB (60.64%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,32 +1,33 @@
 class Solution {
-    private:
-    int call(int cr,int state,int k,vector<int> &p,int n,vector<vector<vector<int>>>&dp)
-{
-    if(cr>=n || k==0)
-    return 0;
-    
-    if(dp[cr][state][k]!=-1)
-    return dp[cr][state][k];
-    int pf=0;
-    // wanna 1 buy
-    if(state==1)
-    {
-        pf=max(call(cr+1,0,k,p,n,dp)-p[cr],call(cr+1,1,k,p,n,dp));
-    }
-    else //sell
-    {
-        pf=max(call(cr+1,0,k,p,n,dp),call(cr+1,1,k-1,p,n,dp)+p[cr]);
-    }
-    return dp[cr][state][k]=pf;
-
-}
 public:
+    int maxProfit(vector<int>& ar) 
+    {
+        int n=ar.size();
+        vector<int>pr(n,0);
+        int mn=ar[0];
+        for(int i=1;i<n;i++)
+        {
+            pr[i]=max(pr[i-1],ar[i]-mn);
+            mn=min(mn,ar[i]);
+        }
+        vector<int>pl(n,0);
+        mn=ar[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            pl[i]=max(pl[i+1],mn-ar[i]);
+            mn=max(mn,ar[i]);
+        }
+        int ans=*max_element(pr.begin(),pr.end());
+        for(auto it:pl)
+        {
+            ans=max(ans,it);
+        }
+        for(int i=1;i<=n-3;i++)
+        {
+            ans=max(pr[i]+pl[i+1],ans);
 
-    int maxProfit(vector<int>& prices) {
-        int k=2;
-        int n=prices.size();
-        // int profit=0;
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,-1)));
-        return call(0,1,k,prices,n,dp);
+        }
+        return ans;
+        
     }
 };
