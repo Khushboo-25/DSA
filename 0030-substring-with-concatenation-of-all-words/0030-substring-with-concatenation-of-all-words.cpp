0a1cc634678b808b073ba0563e5810e47192cc72
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2026-06-29 17:38:19 +0530

    Time: 37 ms (20.11%), Space: 33.1 MB (16.32%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -1,52 +1,50 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        int n = s.size();
-        int m = words.size();
-        int k = words[0].size();
+        int n=s.size();
+        int m=words.size();
+        int k=words[0].size();
 
         vector<int> ans;
-        if (n < m * k) return ans;
+        if(n < m*k) return ans;
 
-        unordered_map<string, int> need;
-        for (auto &it : words)
+        unordered_map<string,int> need;
+        for(auto &it:words)
             need[it]++;
 
+        
         auto check = [&](unordered_map<string,int> &pp) {
-            if (pp.size() != need.size()) return false;
-            for (auto &it : need)
-                if (pp[it.first] != it.second)
+            if(pp.size()!=need.size()) return false;
+            for(auto &it:need)
+                if(pp[it.first]!=it.second)
                     return false;
             return true;
         };
 
-        for (int offset = 0; offset < k; offset++) {
-
-            if (offset + m * k > n) break;
-
+        // if(check()) ans.push_back(0);
+        for(int l=0;l<k;l++)
+        {
+            if(l+m*k>n)
+            break;
             unordered_map<string,int> pp;
 
-            // Build first window
-            for (int i = offset; i < offset + m * k; i += k)
-                pp[s.substr(i, k)]++;
-
+            for(int i=l;i<l+m*k;i+=k )
+            pp[s.substr(i,k)]++;
             if (check(pp))
-                ans.push_back(offset);
+                ans.push_back(l);
 
-            // Slide window
-            for (int i = offset + m * k; i + k <= n; i += k) {
-
-                string rem = s.substr(i - m * k, k);
-                if (--pp[rem] == 0)
+            for(int i=l+m*k;i+k<=n;i+=k)
+            {
+                string rem=s.substr(i-m*k,k);
+                if(--pp[rem]==0)
                     pp.erase(rem);
 
-                pp[s.substr(i, k)]++;
+                pp[s.substr(i,k)]++;
 
-                if (check(pp))
-                    ans.push_back(i - (m - 1) * k);
+                if(check(pp))
+                    ans.push_back(i-(m-1)*k);
             }
         }
-
         return ans;
     }
 };
